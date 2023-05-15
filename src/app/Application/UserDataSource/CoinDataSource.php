18b482639220396a5885feb9f080f52f9d8b337f
Javier <aranguren.126084@e.unavarra.es>
2023-05-15 22:39:44 +0200

    Construcción de la interfaz Coin

--- src/app/Application/UserDataSource/CoinDataSource.php
+++ src/app/Application/UserDataSource/CoinDataSource.php
@@ -6,14 +6,42 @@ use App\Domain\Coin;
 
 Interface CoinDataSource
 {
+    // Atributos
+    /**
+     * @var private string $coin_id;
+     * @var private string $name;
+     * @var private string $symbol;
+     * @var private float $amount;
+     * @var private float $value_usd;
+     */
+
+    // Getters
+    public function getCoin_id();
+    public function getName();
+    public function getSymbol();
+    public function getAmount();
+    public function getValue_usd();
+
+    // Setters
+    public function setCoin_id();
+    public function setName();
+    public function setSymbol();
+    public function setAmount();
+    public function setValue_usd();
+
+    // Funciones
+    /**
+     * Permite comprar $amount_usd cantidad de la moneda.
+     * @param float $amount_usd: Cantidad en dolares a comprar.
+     * @return int: Resultado de la ejecución de la función.
+     */
+    public function buy(float $amount_usd): int;
 
     /**
-     * @return Coin[]
+     * Permite vender $amount_usd cantidad de la moneda.
+     * @param float $amount_usd: Cantidad en dolares a vender.
+     * @return int: Resultado de la ejecución de la función.
      */
-    public function getAll(): array;
-
-    public function buyCoin(string $coin_id, string $wallet_id, float $amount_usd): int;
-
-    public function sellCoin(string $coin_id): int;
+    public function sell(float $amount_usd): int;
 
 }
