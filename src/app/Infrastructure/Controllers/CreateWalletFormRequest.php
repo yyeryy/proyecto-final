6970adcc7f364ec892f2111ce6fcd5a6baa47566
Javier <aranguren.126084@e.unavarra.es>
2023-05-17 09:48:36 +0200

    Activar tetst validar usuario

--- src/app/Infrastructure/Controllers/CreateWalletFormRequest.php
+++ src/app/Infrastructure/Controllers/CreateWalletFormRequest.php
@@ -7,7 +7,7 @@ class CreateWalletFormRequest
     //validamos parametro user_id
     //Recibimos parametro user_id
 
-    /*public function validate(string $user_id){
+    public function validate(string $user_id){
         if(intval($user_id) < 1)
         {
             return "NoOk";
@@ -16,9 +16,9 @@ class CreateWalletFormRequest
         {
             return "Ok";
         }
-    }*/
+    }
 
-    
+
 
 
 }
