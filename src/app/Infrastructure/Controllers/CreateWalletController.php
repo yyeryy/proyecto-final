665a8886c87df7ea78fbd1a561cc96fa78a35986
Yeray Aller <aller.133416@e.unavarra.es>
2023-05-18 11:24:11 +0200

    Inyeccion de dependencias CreateWalletService

--- src/app/Infrastructure/Controllers/CreateWalletController.php
+++ src/app/Infrastructure/Controllers/CreateWalletController.php
@@ -6,10 +6,16 @@ use App\Application\CreateWalletService;
 
 class CreateWalletController
 {
+    public function __construct(CreateWalletService $createWalletService)
+    {
+        $this->createWalletService = $createWalletService;
+    }
     public function __invoke(CreateWalletFormRequest $request)
     {
+        $user_id = $request->safe()->only(['user_id']);
+        $this->createWalletService->execute($user_id);
         return response()->json([
             'status' => 'Ok',
-            ]);
-        }
+        ]);
+    }
 }
