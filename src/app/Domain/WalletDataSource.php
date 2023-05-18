7a4042c88eb84c3d2e3dc89f613f6bdaf82308b8
Yeray Aller <aller.133416@e.unavarra.es>
2023-05-18 15:46:05 +0200

    merge CacheWalletDataSource

--- src/app/Domain/WalletDataSource.php
+++ src/app/Domain/WalletDataSource.php
@@ -6,51 +6,5 @@ use App\Domain\WalletDataSource\private;
 
 Interface WalletDataSource
 {
-    // Atributos
-    /**
-     * @var private string wallet_id; Identificador de la moneda.
-     * @var private Coin coin[]; Lista de monedas.
-     */
 
-    // Getters
-    public function getWallet_id(): string;
-    public function getCoin(): Coin;
-
-    // Setters
-    public function setWallet_id($wallet_id);
-    public function setCoin($coin);
-
-    // Función
-    /**
-     * Muesra (print) la id de la cartera nueva que se crea al llamar a esta función.
-     * @return int: Resultado de la ejecución de la función.
-     *  200: Exito. (Se han mostrado)
-     *  400: Error. (Mete un try-catch)
-     *  401: Error. (El usuario ya tiene una cartera)
-     */
-    public function open(): int;
-
-    /**
-     * Muestra (print) la información de la cartera, mostrando información de la lista de monedas que posee la cartera.
-     * @return int: Resultado de la ejecución de la función.
-     *  200: Exito. (Se han mostrado)
-     *  400: Error. (Mete un try-catch)
-     */
-    public function info(): int;
-
-    /**
-     * Muestra (print) el balance de la cartera (la diferencia entre los precios de compra y los actuales de venta)
-     * @return int: Resultado de la ejecución de la función.
-     *  200: Exito. (Se han mostrado)
-     *  400: Error. (Mete un try-catch)
-     */
-    public function balance(): int;
-
-    /**
-     * Muestra (print) las monedas disponibles para su compra.
-     * @return int: Resultado de la ejecución de la función.
-     *  200: Exito. (Se han mostrado)
-     *  400: Error. (Mete un try-catch)
-     */
-    public function mostrarTop100Monedas(): int;
 }
--- src/app/Infrastructure/Persistence/CacheWalletDataSource.php
+++ src/app/Infrastructure/Persistence/CacheWalletDataSource.php
@@ -10,45 +10,6 @@ use Illuminate\Support\Facades\Cache;
 class CacheWalletDataSource implements WalletDataSource
 {
 
-    public function getWallet_id(): string
-    {
-        // TODO: Implement getWallet_id() method.
-    }
-
-    public function getCoin(): Coin
-    {
-        // TODO: Implement getCoin() method.
-    }
-
-    public function setWallet_id($wallet_id)
-    {
-        // TODO: Implement setWallet_id() method.
-    }
-
-    public function setCoin($coin)
-    {
-        // TODO: Implement setCoin() method.
-    }
-
-    public function open(): int
-    {
-        // TODO: Implement open() method.
-    }
-
-    public function info(): int
-    {
-        // TODO: Implement info() method.
-    }
-
-    public function balance(): int
-    {
-        // TODO: Implement balance() method.
-    }
-
-    public function mostrarTop100Monedas(): int
-    {
-        // TODO: Implement mostrarTop100Monedas() method.
-    }
     public function create(string $userid){
         $walletId = Cache::get('last_wallet_id', 0);
         $newId = $walletId + 1;
