a1ee44a22ec1e98a3fd40a1350ec806cf51d65f9
Javier <aranguren.126084@e.unavarra.es>
2023-05-18 11:09:41 +0200

    Elimino funciones authorize y message dado que no sirven

--- src/app/Infrastructure/Controllers/CreateWalletFormRequest.php
+++ src/app/Infrastructure/Controllers/CreateWalletFormRequest.php
@@ -8,22 +8,10 @@ class CreateWalletFormRequest extends FormRequest
 {
     //validamos parametro user_id
     //Recibimos parametro user_id
-    public function authorize()
-    {
-        return true;
-    }
-
     public function rules()
     {
         return [
             'user_id' => 'required|string'
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-            "user_id.required" => "ERROR: Parametros incorrectos"
         ];
     }
 }
