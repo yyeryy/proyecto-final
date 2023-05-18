a905cdd57da617bfc3e8512a5948104717ec8002
Yeray Aller <aller.133416@e.unavarra.es>
2023-05-18 17:38:34 +0200

    Creacion BuyCoinController

--- src/app/Infrastructure/Controllers/BuyCoinController.php
+++ src/app/Infrastructure/Controllers/BuyCoinController.php
@@ -2,14 +2,22 @@
 
 namespace App\Infrastructure\Controllers;
 
+use App\Application\BuyCoinService;
 use http\Env\Request;
 use Illuminate\Support\Facades\Validator;
 
 class BuyCoinController
 {
-    public function __invoke(Request $request)
+
+    private BuyCoinService $buyCoinService;
+
+    public function __construct(BuyCoinService $buyCoinService)
     {
-        $validator = Validator::make($request->all(), [
+        $this->buyCoinService = $buyCoinService;
+    }
+    public function __invoke(BuyCoinFormRequest $request)
+    {
+        /*$validator = Validator::make($request->all(), [
             'coin_id' => 'required|string',
             'wallet_id' => 'required|string',
             'amount_usd' => 'required|numeric',
@@ -19,6 +27,16 @@ class BuyCoinController
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        return response()->json(['message' => 'Compra de moneda exitosa'], 200);
+        return response()->json(['message' => 'Compra de moneda exitosa'], 200);*/
+
+        $validatedData = $request->validated();
+
+        try {
+            $this->buyCoinService->execute($validatedData);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => $e->getMessage()
+            ]);
+        }
     }
 }
