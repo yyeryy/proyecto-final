de361694f0f656004afadf79b8d910faa03b9574
Yeray Aller <aller.133416@e.unavarra.es>
2023-05-20 12:12:29 +0200

    Finalinalizado ramas

---
+++ src/tests/app/Infrastructure/Controller/BuyCoinControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Infrastructure\Controllers;
+
+use App\Infrastructure\Controllers\BuyCoinController;
+use PHPUnit\Framework\TestCase;
+
+class BuyCoinControllerTest extends TestCase
+{
+
+}
