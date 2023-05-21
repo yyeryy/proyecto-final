0f7e97f12cfba6b56c7b8219d444908e18a60026
Yeray Aller <aller.133416@e.unavarra.es>
2023-05-21 21:02:41 +0200

    Create WalletBalanceControllerTest

--- src/tests/app/Infrastructure/Controller/WalletBalanceControllerTest.php
+++ src/tests/app/Infrastructure/Controller/WalletBalanceControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\app\Infrastructure\Controller;
 
+use Exception;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use App\Application\WalletBalanceService;
 use App\Infrastructure\Controllers\WalletBalanceController;
@@ -14,22 +16,61 @@ use Illuminate\Support\Facades\Response;
 
 class WalletBalanceControllerTest extends TestCase
 {
-    /**
-
-    @test*/
-    public function test_wallet_balance_controller()
+    protected function setUp(): void
     {
-        $walletBalanceService = new WalletBalanceService(new CacheWalletDataSource(), new APICoinDataSource());
-        $walletBalanceController = new WalletBalanceController($walletBalanceService);
+        parent::setUp();
+        $this->WalletBalanceController = Mockery::mock(WalletBalanceController::class);
+    }
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+    /**
+     * @test
+     */
+    public function invalid_wallet_Id_test()
+    {
+        $requestData = json_encode(array('wallet_id' => '1'));
+        $request = Request::create('/wallet/h/balance', 'POST', [],[],[],[],$requestData);
+        $this->WalletBalanceController->shouldReceive('__invoke')
+            ->once()
+            ->with($request, 'h')
+            ->andReturn(new JsonResponse(['status' => 'ERROR: Los parametros introducidos no son validos.']));
+        $result = $this->WalletBalanceController->__invoke($request, 'h');
+        $this->assertInstanceOf(JsonResponse::class, $result);
+        $this->assertJsonStringEqualsJsonString('{"status": "ERROR: Los parametros introducidos no son validos."}', $result->content());
+    }
 
-        $data = array('wallet_id' => '1');
-        $requestData = json_encode($data);
+    /**
+     * @test
+     */
+    public function get_correctly_balance_test()
+    {
+        $requestData = json_encode(array('wallet_id' => '1'));
+        $request = Request::create('/wallet/1/balance', 'POST', [],[],[],[],$requestData);
+        $this->WalletBalanceController->shouldReceive('__invoke')
+            ->once()
+            ->with($request, '1')
+            ->andReturn(new JsonResponse(['Balance' => '500$']));
+        $result = $this->WalletBalanceController->__invoke($request, '1');
+        $this->assertInstanceOf(JsonResponse::class, $result);
+        $this->assertJsonStringEqualsJsonString('{"Balance": "500$"}', $result->content());
+    }
 
-        $request = Request::create('/coin/sell', 'POST', [],[],[],[],$requestData);
-
-        $response = ($walletBalanceController)->__invoke($request, 'hola');
-
-        $this->assertExactJson(['status' => 'ERROR: Los parametros introducidos no son validos.']);
-        //$this->assertEquals('Hola', $response);
+    /**
+     * @test
+     */
+    public function invalid_wallet_Id_throw_exception_test()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Wallet Not found exception");
+        $requestData = json_encode(array('wallet_id' => '2'));
+        $request = Request::create('/wallet/2/balance', 'POST', [],[],[],[],$requestData);
+        $this->WalletBalanceController->shouldReceive('__invoke')
+            ->once()
+            ->with($request, '2')
+            ->andThrow(new Exception("Wallet Not found exception"));
+        $this->WalletBalanceController->__invoke($request, '2');
     }
 }
