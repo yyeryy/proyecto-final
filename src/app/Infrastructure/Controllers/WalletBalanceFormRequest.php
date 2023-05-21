2a955e057f260b42a547646c4e00395f21eabf01
Julen Huarte <huarte.133745@e.unavarra.es>
2023-05-21 21:39:08 +0200

    Requisitos en método rules modificados en WalletBallanceFormRequest

--- src/app/Infrastructure/Controllers/WalletBalanceFormRequest.php
+++ src/app/Infrastructure/Controllers/WalletBalanceFormRequest.php
@@ -2,7 +2,16 @@
 
 namespace App\Infrastructure\Controllers;
 
-class WalletBalanceFormRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class WalletBalanceFormRequest extends FormRequest
+{
+    public function rules()
+    {
+        return[
+            'coin_id' => 'required|string',
+            'wallet_id' => 'required|string',
+            'amount_usd' => 'required|numeric'
+        ];
+    }
 }
