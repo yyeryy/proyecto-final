aeb0e4a3804d3200150be097791efad9f1b032b2
Luis4444A <azcoitiluis4444@gmail.com>
2023-05-21 16:50:12 +0200

    Test SellCoinController

--- src/tests/app/Infrastructure/Controller/SellCoinControllerTest.php
+++ src/tests/app/Infrastructure/Controller/SellCoinControllerTest.php
@@ -7,16 +7,42 @@ use App\Infrastructure\Controllers\SellCoinController;
 use App\Infrastructure\Persistence\CacheWalletDataSource;
 use App\Infrastructure\Persistence\APICoinDataSource;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use PHPUnit\Framework\TestCase;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SellCoinControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
-    public function validate_request_returns_error_if_amount_usd_is_smaller_than_cero()
+    public function testValidationFailsWithInvalidData()
+    {
+        // Configurar las dependencias necesarias
+        $apiCoinDataSource = $this->getMockBuilder(APICoinDataSource::class)->getMock();
+        $cacheWalletDataSource = $this->getMockBuilder(CacheWalletDataSource::class)->getMock();
+        $sellCoinService = new SellCoinService($cacheWalletDataSource, $apiCoinDataSource);
+
+        // Crear una instancia del controlador
+        $controller = new SellCoinController($sellCoinService);
+
+        // Crear una solicitud falsa con datos de prueba inválidos
+        $request = Request::create('/api/sell-coin', 'POST', [
+            'coin_id' => '',
+            'wallet_id' => '123',
+            'amount_usd' => 'abc',
+        ]);
+
+        // Ejecutar la función __invoke() del controlador
+        $response = $controller->__invoke($request);
+
+        // Verificar el estado de respuesta y los errores de validación
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['coin_id']);
+    }
+
+    /*public function validate_request_returns_error_if_amount_usd_is_smaller_than_cero()
     {
         $sellCoinService = new SellCoinService(new CacheWalletDataSource(), new APICoinDataSource());
         $sellCoinController = new SellCoinController($sellCoinService);
@@ -34,5 +60,5 @@ class SellCoinControllerTest extends TestCase
         $response = ($sellCoinController)->__invoke($request);
 
         //$result->assertExactJson(['errors' => 'El amount no puede ser menor o igual a 0']);
-    }
+    }*/
 }
