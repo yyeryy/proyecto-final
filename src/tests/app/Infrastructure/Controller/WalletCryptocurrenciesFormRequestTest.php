35cb8fb3f1d889966bd27754ce569c66de0bd93f
Julen Huarte <huarte.133745@e.unavarra.es>
2023-05-21 23:15:39 +0200

    Arreglo de fallos en WalletCryptocurrenciesFormRequestTest

--- src/tests/app/Infrastructure/Controller/WalletCryptocurrenciesFormRequestTest.php
+++ src/tests/app/Infrastructure/Controller/WalletCryptocurrenciesFormRequestTest.php
@@ -12,7 +12,7 @@ class WalletCryptocurrenciesFormRequestTest extends TestCase
      */
     public function formRequest_validates_coin_id_parameter()
     {
-        $createWalletFormRequest = new CreateWalletFormRequest();
+        $createWalletFormRequest = new WalletCryptocurrenciesFormRequest();
 
         $rules = $createWalletFormRequest->rules();
         $this->assertArrayHasKey('coin_id', $rules);
@@ -25,7 +25,7 @@ class WalletCryptocurrenciesFormRequestTest extends TestCase
      */
     public function formRequest_validates_name_parameter()
     {
-        $createWalletFormRequest = new CreateWalletFormRequest();
+        $createWalletFormRequest = new WalletCryptocurrenciesFormRequest();
 
         $rules = $createWalletFormRequest->rules();
         $this->assertArrayHasKey('name', $rules);
@@ -38,7 +38,7 @@ class WalletCryptocurrenciesFormRequestTest extends TestCase
      */
     public function formRequest_validates_symbol_parameter()
     {
-        $createWalletFormRequest = new CreateWalletFormRequest();
+        $createWalletFormRequest = new WalletCryptocurrenciesFormRequest();
 
         $rules = $createWalletFormRequest->rules();
         $this->assertArrayHasKey('symbol', $rules);
@@ -51,7 +51,7 @@ class WalletCryptocurrenciesFormRequestTest extends TestCase
      */
     public function formRequest_validates_amount_parameter()
     {
-        $createWalletFormRequest = new CreateWalletFormRequest();
+        $createWalletFormRequest = new WalletCryptocurrenciesFormRequest();
 
         $rules = $createWalletFormRequest->rules();
         $this->assertArrayHasKey('amount', $rules);
@@ -64,7 +64,7 @@ class WalletCryptocurrenciesFormRequestTest extends TestCase
      */
     public function formRequest_validates_value_usd_parameter()
     {
-        $createWalletFormRequest = new CreateWalletFormRequest();
+        $createWalletFormRequest = new WalletCryptocurrenciesFormRequest();
 
         $rules = $createWalletFormRequest->rules();
         $this->assertArrayHasKey('value_usd', $rules);
