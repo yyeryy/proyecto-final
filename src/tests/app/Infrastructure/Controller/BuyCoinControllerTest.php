e8fd1c7cde816efcc433b5266c47f0009ba54750
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-07 16:52:01 +0200

    Modificacion del BuyCoinControllerTest creando correctamente el invalid_amount_usd_less_than_zero_or_equal_test

--- src/tests/app/Infrastructure/Controller/BuyCoinControllerTest.php
+++ src/tests/app/Infrastructure/Controller/BuyCoinControllerTest.php
@@ -34,4 +34,23 @@ class BuyCoinControllerTest extends TestCase
         $this->assertInstanceOf(JsonResponse::class, $result);
         $this->assertJsonStringEqualsJsonString('{"status": "Compra realizada"}', $result->content());
     }
+
+    /**
+     * @test
+     */
+    public function invalid_amount_usd_less_than_zero_or_equal_test(){
+        $this->buyCoinServiceMock = Mockery::mock(BuyCoinService::class);
+
+        $request = Request::create('/coin/buy', 'POST', [
+            'coin_id' => '90',
+            'wallet_id' => '1',
+            'amount_usd' => -100
+        ]);
+
+        $BuyCoinController = new BuyCoinController($this->buyCoinServiceMock);
+        $result = $BuyCoinController->__invoke($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $result);
+        $this->assertJsonStringEqualsJsonString('{"errors": "El amount no puede ser menor o igual que 0"}', $result->content());
+    }
 }
