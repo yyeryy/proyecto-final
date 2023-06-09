cc42d55084c8343ad87297fdff3f4356a627b22d
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-09 23:55:37 +0200

    -Actualizacion CacheWalletDataSource y CacheWalletDataSourceTes Luis

--- src/app/Infrastructure/Persistence/CacheWalletDataSource.php
+++ src/app/Infrastructure/Persistence/CacheWalletDataSource.php
@@ -5,18 +5,27 @@ namespace App\Infrastructure\Persistence;
 use App\Domain\Coin;
 use App\Domain\Wallet;
 use App\Domain\WalletDataSource;
-use Illuminate\Support\Facades\Cache;
+//use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
 use PHPUnit\Util\Exception;
 
 class CacheWalletDataSource implements WalletDataSource
 {
+    protected $cache;
+
+    public function __construct(CacheRepository $cache)
+    {
+        $this->cache = $cache;
+    }
+
     public function createWallet(string $userid)
     {
-        $wallet = Cache::get('wallet:' . $userid);
-        //$wallet = $this->findById($userid);
-        if(!$wallet) {
+        //$wallet = Cache::get('wallet:' . $userid);
+        $wallet = $this->cache->get('wallet:' . $userid);
+        if (!$wallet) {
             $wallet = new Wallet($userid);
-            Cache::put('wallet:' . $userid, $wallet);
+            //Cache::put('wallet:' . $userid, $wallet);
+            $this->cache->put('wallet:' . $userid, $wallet);
             return $wallet;
         }
         return $wallet;
@@ -24,7 +33,8 @@ class CacheWalletDataSource implements WalletDataSource
 
     public function findById(string $walletId)
     {
-        $wallet = Cache::get('wallet:' . $walletId);
+        //$wallet = Cache::get('wallet:' . $walletId);
+        $wallet = $this->cache->get('wallet:' . $walletId);
         if ($wallet) {
             return $wallet;
         }
