23d40d62287adc3b43f22a17b3810a069294f89d
Yeray Aller <aller.133416@e.unavarra.es>
2023-06-09 10:50:09 +0200

    -Correccion de la funcionalidad SellCoin Yeray

--- src/app/Domain/Wallet.php
+++ src/app/Domain/Wallet.php
@@ -80,6 +80,8 @@ class Wallet
             if($this->coins[$key]->getAmount() > $coin->getAmount()){
                 $this->coins[$key]->setAmount($this->coins[$key]->getAmount() - $coin->getAmount());
                 $this->coins[$key]->setValueUsd($this->coins[$key]->getValueUsd() - $coin->getValueUsd());
+            } else if($this->coins[$key]->getAmount() == $coin->getAmount()){
+                array_splice($this->coins, $key,1);
             } else{
                 throw new Exception("No se puede vender mas de lo que tienes");
             }
