1e626062b162f070833bc858b22f90ffbb724b95
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-12 17:32:20 +0200

    -Actualizacion WalletBalanceServiceTest para pasar code sniffer y mess detector Luis

--- src/tests/app/Application/WalletBalanceServiceTest.php
+++ src/tests/app/Application/WalletBalanceServiceTest.php
@@ -13,27 +13,32 @@ use Mockery;
 use App\Domain\Wallet;
 use App\Application\WalletBalanceService;
 
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
 class WalletBalanceServiceTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        //$this->WalletBalanceServiceMock = Mockery::mock(WalletBalanceService::class);
         $this->APICoinDataSourceMock = Mockery::mock(APICoinDataSource::class);
         $this->CacheWalletDataSourceMock = Mockery::mock(CacheWalletDataSource::class);
     }
-    protected function tearDown(): void
-    {
-        Mockery::close();
-        parent::tearDown();
-    }
+
     /**
      * @test
      */
-    public function execute_create_balance_with_no_coins_test(){
+    public function executeCreateBalanceWithNoCoinsTest()
+    {
         $wallet = new Wallet('1');
-        $this->CacheWalletDataSourceMock->shouldReceive('findById')->once()->with('1')->andReturn($wallet);
-        $walletBalanceService = new WalletBalanceService($this->CacheWalletDataSourceMock, $this->APICoinDataSourceMock);
+        $this->CacheWalletDataSourceMock->shouldReceive('findById')
+            ->once()
+            ->with('1')
+            ->andReturn($wallet);
+        $walletBalanceService = new WalletBalanceService(
+            $this->CacheWalletDataSourceMock,
+            $this->APICoinDataSourceMock
+        );
 
         $this->expectException(\Exception::class);
 
@@ -43,14 +48,24 @@ class WalletBalanceServiceTest extends TestCase
     /**
      * @test
      */
-    public function execute_create_balance_return_balance_test(){
+    public function executeCreateBalanceReturnBalanceTest()
+    {
         $wallet = new Wallet('1');
         $coin = new Coin(90, 'Bitcoin', 'BTC', 2, 500, 1);
         $wallet->insertCoin($coin);
-        $this->CacheWalletDataSourceMock->shouldReceive('findById')->once()->with('1')->andReturn($wallet);
-        $this->APICoinDataSourceMock->shouldReceive('getBalanceById')->once()->with('90,0')->andReturn('500');
+        $this->CacheWalletDataSourceMock->shouldReceive('findById')
+            ->once()
+            ->with('1')
+            ->andReturn($wallet);
+        $this->APICoinDataSourceMock->shouldReceive('getBalanceById')
+            ->once()
+            ->with('90,0')
+            ->andReturn('500');
 
-        $walletBalanceService = new WalletBalanceService($this->CacheWalletDataSourceMock, $this->APICoinDataSourceMock);
+        $walletBalanceService = new WalletBalanceService(
+            $this->CacheWalletDataSourceMock,
+            $this->APICoinDataSourceMock
+        );
 
         $response = $walletBalanceService->execute('1');
         $this->assertEquals('-490$', $response);
