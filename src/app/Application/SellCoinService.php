c926116461968036335d91dc123cf4f3ba246545
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-12 18:28:34 +0200

    -Actualizacion SellCoinService para pasar code sniffer y mess detector Luis

--- src/app/Application/SellCoinService.php
+++ src/app/Application/SellCoinService.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Cache;
 use App\Infrastructure\Persistence\APICoinDataSource;
 use App\Infrastructure\Persistence\CacheWalletDataSource;
 
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
 class SellCoinService
 {
-    private CacheWalletDataSource $cacheWalletDataSource;
+    private CacheWalletDataSource $cacheWallet;
     private APICoinDataSource $apiCoinDataSource;
 
-    public function __construct(CacheWalletDataSource $cacheWalletDataSource, APICoinDataSource $apiCoinDataSource)
+    public function __construct(CacheWalletDataSource $cacheWallet, APICoinDataSource $apiCoinDataSource)
     {
-        $this->cacheWalletDataSource = $cacheWalletDataSource;
+        $this->cacheWallet = $cacheWallet;
         $this->apiCoinDataSource = $apiCoinDataSource;
     }
 
     public function execute($coinId, $walletId, $amountUsd): void
     {
-        $wallet = $this->cacheWalletDataSource->findById($walletId);
+        $wallet = $this->cacheWallet->findById($walletId);
         $coin = $this->apiCoinDataSource->getById($coinId, $amountUsd);
         $wallet->sellCoin($coin);
         Cache::put('wallet:' . $walletId, $wallet);
