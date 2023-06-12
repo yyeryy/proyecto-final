e3c68ef35cb68f6c54cdce4bea54ad85c772fac3
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-12 17:59:15 +0200

    -Actualizacion CacheUserDataSource para pasar code sniffer y mess detector Luis

--- src/app/Infrastructure/Persistence/CacheUserDataSource.php
+++ src/app/Infrastructure/Persistence/CacheUserDataSource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Infrastructure\Persistence;
+
 use App\Domain\User;
 use App\Domain\UserDataSource;
 use App\Domain\Wallet;
@@ -9,6 +10,10 @@ use PHPUnit\Util\Exception;
 
 define("UNIQUE_USER_ID", "1");
 
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ * @SuppressWarnings(PHPMD.ElseExpression)
+ */
 class CacheUserDataSource implements UserDataSource
 {
     //Obtenemos usuario de caché.
@@ -17,8 +22,7 @@ class CacheUserDataSource implements UserDataSource
         // Comprobar si el usuario es 1, único usuario válido.
 
         $user = Cache::get('user:' . UNIQUE_USER_ID);
-        if(!$user)
-        {
+        if (!$user) {
             throw new Exception("User Not found exception");
         }
         return $user;
@@ -27,16 +31,15 @@ class CacheUserDataSource implements UserDataSource
     public function findUserById(string $userId)
     {
         $user = Cache::get('user:' . $userId);
-        if($userId == "1"){
+        if ($userId == "1") {
             if ($user) {
                 return $user;
-            }else {
+            } else {
                 Cache::put('user:' . $userId, $userId);
                 $user = Cache::get('user:' . $userId);
                 return $user;
             }
         }
-
         return null;
     }
 }
