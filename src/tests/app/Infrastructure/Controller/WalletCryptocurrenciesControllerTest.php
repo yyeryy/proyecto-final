68a0d03246ab9efde1ff908cbe6859bfd4f8c55b
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-12 17:10:11 +0200

    -Actualizacion WalletCryptocurrenciesControllerTest para pasar code sniffer y mess detector Luis

--- src/tests/app/Infrastructure/Controller/WalletCryptocurrenciesControllerTest.php
+++ src/tests/app/Infrastructure/Controller/WalletCryptocurrenciesControllerTest.php
@@ -12,43 +12,47 @@ use Tests\TestCase;
 use Exception;
 use Mockery;
 
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
 class WalletCryptocurrenciesControllerTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
         $this->WalletCryptocurrenciesServiceMock = Mockery::mock(WalletCryptocurrenciesService::class);
-        $this->WalletCryptocurrenciesController = new WalletCryptocurrenciesController($this->WalletCryptocurrenciesServiceMock);
+        $this->WalletCryptocurrenciesController = new WalletCryptocurrenciesController(
+            $this->WalletCryptocurrenciesServiceMock
+        );
     }
-    protected function tearDown(): void
-    {
-        Mockery::close();
-        parent::tearDown();
-    }
+
     /**
      * @test
      */
-    public function invalid_wallet_id_test()
+    public function invalidWalletIdTest()
     {
         $requestData = json_encode(array('wallet_id' => 'h'));
-        $request = Request::create('/wallet/h', 'GET', [],[],[],[],$requestData);
+        $request = Request::create('/wallet/h', 'GET', [], [], [], [], $requestData);
 
         $result = $this->WalletCryptocurrenciesController->__invoke($request, 'h');
 
         $this->assertInstanceOf(JsonResponse::class, $result);
-        $this->assertJsonStringEqualsJsonString('{"status": "ERROR: Los parametros introducidos no son validos."}', $result->content());
+        $this->assertJsonStringEqualsJsonString(
+            '{"status": "ERROR: Los parametros introducidos no son validos."}',
+            $result->content()
+        );
     }
 
     /**
      * @test
      */
-    public function get_correctly_cryptocurrencies_test()
+    public function getCorrectlyCryptocurrenciesTest()
     {
         $wallet = new Wallet('1');
         $coin = new Coin(90, 'Bitcoin', 'BTC', 1, 27000, 1);
         $wallet->insertCoin($coin);
         $requestData = json_encode(array('wallet_id' => '1'));
-        $request = Request::create('/wallet/1', 'GET', [],[],[],[],$requestData);
+        $request = Request::create('/wallet/1', 'GET', [], [], [], [], $requestData);
         $this->WalletCryptocurrenciesServiceMock->shouldReceive('execute')
             ->once()
             ->with('1')
@@ -57,16 +61,19 @@ class WalletCryptocurrenciesControllerTest extends TestCase
         $result = $this->WalletCryptocurrenciesController->__invoke($request, '1');
 
         $this->assertInstanceOf(JsonResponse::class, $result);
-        $this->assertJsonStringEqualsJsonString('{"data":[{"coin_id":90,"name":"Bitcoin","symbol":"BTC","amount":1,"value_usd":27000}]}', $result->content());
+        $this->assertJsonStringEqualsJsonString(
+            '{"data":[{"coin_id":90,"name":"Bitcoin","symbol":"BTC","amount":1,"value_usd":27000}]}',
+            $result->content()
+        );
     }
 
     /**
      * @test
      */
-    public function invalid_wallet_id_throw_exception_test()
+    public function invalidWalletIdThrowExceptionTest()
     {
         $requestData = json_encode(array('wallet_id' => '2'));
-        $request = Request::create('/wallet/2', 'GET', [],[],[],[],$requestData);
+        $request = Request::create('/wallet/2', 'GET', [], [], [], [], $requestData);
         $this->WalletCryptocurrenciesServiceMock->shouldReceive('execute')
             ->once()
             ->with('2')
