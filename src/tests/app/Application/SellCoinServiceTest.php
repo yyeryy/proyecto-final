5d2158c190b0bfa7c77e2a0aa9db1d4870f48427
Luis4444A <azcoitiluis4444@gmail.com>
2023-06-12 17:35:38 +0200

    -Actualizacion SellCoinServiceTest para pasar code sniffer y mess detector Luis

--- src/tests/app/Application/SellCoinServiceTest.php
+++ src/tests/app/Application/SellCoinServiceTest.php
@@ -11,16 +11,23 @@ use Illuminate\Support\Facades\Cache;
 use PHPUnit\Framework\TestCase;
 use Mockery;
 
+/**
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
 class SellCoinServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function execute_sell_coin_with_not_existing_wallet_id_test(){
+    public function executeSellCoinWithNotExistingWalletIdTest()
+    {
         $this->cacheWalletDataSourceMock = Mockery::mock(CacheWalletDataSource::class);
         $this->APICoinDataSourceMock = Mockery::mock(APICoinDataSource::class);
 
-        $this->cacheWalletDataSourceMock->shouldReceive('findById')->once()->with('2')->andReturn(null);
+        $this->cacheWalletDataSourceMock->shouldReceive('findById')
+            ->once()
+            ->with('2')
+            ->andReturn(null);
 
         $sellCoinService = new SellCoinService($this->cacheWalletDataSourceMock, $this->APICoinDataSourceMock);
 
@@ -32,7 +39,8 @@ class SellCoinServiceTest extends TestCase
     /**
      * @test
      */
-    public function execute_sell_coin_with_existing_wallet_id_test(){
+    public function executeSellCoinWithExistingWalletIdTest()
+    {
         $wallet = new Wallet('1');
         $walletId = '1';
         $coin = new Coin(90, 'Bitcoin', 'BTC', 2, 500, 1);
@@ -40,18 +48,31 @@ class SellCoinServiceTest extends TestCase
         $this->cacheWalletDataSourceMock = Mockery::mock(CacheWalletDataSource::class);
         $this->APICoinDataSourceMock = Mockery::mock(APICoinDataSource::class);
         $this->WalletMock = Mockery::mock(Wallet::class);
-        $this->cacheMock = Mockery::mock('alias:'.Cache::class);
+        $this->cacheMock = Mockery::mock('alias:' . Cache::class);
 
-        $this->cacheWalletDataSourceMock->shouldReceive('findById')->once()->with('1')->andReturn($this->WalletMock);
-        $this->APICoinDataSourceMock->shouldReceive('getById')->once()->with('90', 500)->andReturn($coin);
-        $this->WalletMock->shouldReceive('sellCoin')->once()->with($coin);
-        $this->cacheMock->shouldReceive('put')->once()->with('wallet:' . $walletId, Mockery::type(Wallet::class));
-        $this->cacheMock->shouldReceive('get')->once()->with('wallet:' . $walletId)->andReturn($wallet);
+        $this->cacheWalletDataSourceMock->shouldReceive('findById')
+            ->once()
+            ->with('1')
+            ->andReturn($this->WalletMock);
+        $this->APICoinDataSourceMock->shouldReceive('getById')
+            ->once()
+            ->with('90', 500)
+            ->andReturn($coin);
+        $this->WalletMock->shouldReceive('sellCoin')
+            ->once()
+            ->with($coin);
+        $this->cacheMock->shouldReceive('put')
+            ->once()
+            ->with('wallet:' . $walletId, Mockery::type(Wallet::class));
+        $this->cacheMock->shouldReceive('get')
+            ->once()
+            ->with('wallet:' . $walletId)
+            ->andReturn($wallet);
 
         $sellCoinService = new SellCoinService($this->cacheWalletDataSourceMock, $this->APICoinDataSourceMock);
         $sellCoinService->execute(90, '1', 500);
 
         $cachedWallet = $this->cacheMock->get('wallet:' . $walletId);
-        $this->assertSame($cachedWallet,$wallet);
+        $this->assertSame($cachedWallet, $wallet);
     }
 }
